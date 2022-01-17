44ba054f35e3f15fc91359ef3a115690d2af6e82
John3107-front <97689679+John3107-front@users.noreply.github.com>
2022-01-17 20:07:35 +0200

    Revert "Develop"

--- src/LTE/Containers/Vx/Header.vue
+++ src/LTE/Containers/Vx/Header.vue
@@ -4,46 +4,13 @@
       <div class="vymex-logo">
         <img src="@/assets/img/vymex/base-reverse.svg" alt="">
       </div>
-      <div class="header-icons">
-        <div
-          class="header-icon icon-alarm"
-          @click="getNotify"
-        >
-          <icon-alarm/>
-        </div>
-        <div class="header-icon icon-comments"><icon-comments/></div>
-      </div>
     </div>
-    <vx-notifications
-      :notify="notify"
-      @get-notify="getNotify"
-    />
-    <div class="opacity" v-if="notify"></div>
   </div>
 </template>
 
 <script>
-  import IconAlarm from '@Icon/Alarm'
-  import IconComments from '@Icon/Comments'
-  import VxNotifications from '@Container/Vx/Notifications'
-
   export default {
     name: 'Container.Vx.Header',
-    components: {
-      IconAlarm,
-      IconComments,
-      VxNotifications
-    },
-    data() {
-      return {
-        notify: false
-      };
-    },
-    methods: {
-      getNotify () {
-        this.notify = !this.notify;
-      }
-    }
   }
 </script>
 
@@ -55,37 +22,12 @@
     background-color: $grey-scale-500;
     .vx-header-body{
       padding: 0 20px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
       .vymex-logo{
         height: 40px;
         img{
           height: 100%;
         }
       }
-      .header-icons {
-        display: flex;
-        .header-icon {
-          padding: 10px;
-          cursor: pointer;
-          height: 16px;
-        }
-        .icon-alarm {
-          width: 16px;
-          margin-right: 12px;
-        }
-      }
-    }
-    .opacity {
-      width: 100%;
-      height: 100%;
-      z-index: 2;
-      top: 0;
-      left: 0;
-      position: absolute;
-      background-color: $grey-scale-700;
-      opacity: 80%;
     }
   }
 </style>
