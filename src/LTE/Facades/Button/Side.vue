2feb7db1d76b18902e41bf84b1eb6c660308beeb
Lera Permiakova <vv.permiakova@gmail.com>
2022-01-18 12:43:25 +0200

    buttons added

---
+++ src/LTE/Facades/Button/Side.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="side-button side-button-special">
+    <slot/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .side-button {
+    width: 72px;
+    height: 65px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: $grey-scale-200;
+    cursor: pointer;
+  }
+</style>
