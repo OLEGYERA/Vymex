fa4f561f129fb1d274a422943c5be53e7031d972
OLEGYERA <olegyera@gmail.com>
2022-02-16 14:45:08 +0200

    Add ListHeader, ButtonAdd

---
+++ src/LTE/Facades/Navigation/ListHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="facade-navigation-list-header">
+    <title-caps>
+      {{title}}
+      <span class="list-title-counter" v-if="titleCount">{{titleCount}}</span>
+    </title-caps>
+    <button-add @onClick="$emit('onAction')"/>
+  </div>
+</template>
+
+<script>
+  import TitleCaps from '@Facade/Title/Caps'
+  import ButtonAdd from '@Facade/Button/Add'
+
+  export default {
+    name: 'Facades.Navigation.ListHeader',
+    components: {
+      TitleCaps, ButtonAdd
+    },
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      titleCount: Number
+
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .facade-navigation-list-header{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .list-title-counter{
+      color: $blue;
+    }
+  }
+</style>
